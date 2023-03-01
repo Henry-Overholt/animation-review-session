a3920e789e9dff10be13150ad6d5e5c4c6d75f3e
Henry Overholt <henrysoverholt@gmail.com>
2023-03-01 13:37:46 -0500

    finished animation lesson with burger icon added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-(() => {})();
+(() => {
+  const icon = document.getElementById("icon");
+  document.getElementById("shape").addEventListener("click", (event) => {
+    console.log(event);
+    if (event.target.style.animationPlayState === "paused") {
+      event.target.style.animationPlayState = "running";
+    } else {
+      event.target.style.animationPlayState = "paused";
+    }
+  });
+  document.body.addEventListener("mousemove", (event) => {
+    icon.style.left = `${event.pageX}px`;
+    icon.style.top = `${event.pageY}px`;
+  });
+  document.body.addEventListener("click", (event) => {
+    icon.classList.add("spin");
+    setTimeout(() => {
+      icon.classList.remove("spin");
+    }, 550);
+    // icon.style.transform = "rotate(360deg)";
+  });
+})();
